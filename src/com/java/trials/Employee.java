9c709d3de727e5fb744003ff28f0c79951e11971
shivali.pawar <shivali.pawar@cerence.com>
2022-05-10 23:03:59 +0530

    Hashcode and equals method in java.

---
+++ src/com/java/trials/Employee.java
@@ -0,0 +1,42 @@
+package com.java.trials;
+
+import java.util.Objects;
+
+public class Employee {
+
+    private int id;
+    private String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && name.equals(employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    public static void main(String[] args) {
+        Employee employee = new Employee(1,"aa");
+        Employee employee1 = new Employee(1,"aa");
+
+        if(employee.hashCode() == employee1.hashCode()){
+            if (employee.equals(employee1)){
+                System.out.println("Both are equal");
+            }else {
+                System.out.println("Both are different");
+            }
+        }else {
+            System.out.println("Hashcode doesn't match");
+        }
+    }
+}
