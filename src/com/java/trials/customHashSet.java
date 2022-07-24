dbbf4d467f763d327f51544777e7162bed9cf4a1
shivali.pawar <shivali.pawar@cerence.com>
2022-07-24 16:55:52 +0530

    Some changes to singleton added.

---
+++ src/com/java/trials/customHashSet.java
@@ -0,0 +1,20 @@
+package com.java.trials;
+
+import java.util.HashSet;
+
+public class customHashSet {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(10);
+        set.add(10);
+        set.add(20);
+//        System.out.println(set.size());
+
+        HashSet<Employee> employeeSet = new HashSet<>();
+        Employee e1 = new Employee(1,"Sam");
+        Employee e2 = new Employee(1,"Sam");
+        employeeSet.add(e1);
+        employeeSet.add(e2);
+        System.out.println(employeeSet.size());
+    }
+}
--- src/com/java/trials/designpattern/SingletonPattern.java
+++ src/com/java/trials/designpattern/SingletonPattern.java
@@ -3,21 +3,27 @@ package com.java.trials.designpattern;
 public class SingletonPattern {
     public static void main(String[] args) {
         Train t = Train.getInstance();
+        Train t2 = Train.getInstance();
         t.setId(1165);
         t.setName("Goa Express");
         System.out.println("Train name is "+t.getName() +" and id is "+t.getId());
+        System.out.println("Both reference same "+(t==t2));
     }
 }
 
 class Train{
     private String name;
     private int id;
+    private static Train train;
 
     private Train(){
 
     }
-   public static Train getInstance(){
-       return new Train();
+   public static synchronized Train getInstance(){
+       if(train == null){
+           train = new Train();
+       }
+       return train;
    }
 
     public String getName() {
